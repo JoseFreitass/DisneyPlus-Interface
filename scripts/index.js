03367b7014a257e345423b6b42a660c0a59bfaf8
JoseFreitass <devjosefreitas@gmail.com>
2022-07-22 01:14:41 -0300

    Api + Mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,63 +1,176 @@
-const API_KEY = '36f6b969572b120032312f924a92e7e6'
+const API_KEY = '03c4e3dc470296959d6bf68804146538'
 const API_LANGUAGE = 'pt-br'
-const BASE_URL_IMAGE = 'https://image.tmdb.org/t/p/original'
-const LIST_MOVIES = ['tt2245084','tt1453405','tt2948356','tt0398286','tt2294629','tt3606756','tt1217209','tt0317219','tt2277860','tt3521164','tt1323594']
-
-
-const moviesList = document.getElementById('movies__list')
-
-function getUrlMovie(movieid){
-    return `https://api.themoviedb.org/3/movie/${movieid}?api_key=${API_KEY}&language=${API_LANGUAGE}`
+const BASE_URL_IMAGE = {
+  original: 'https://image.tmdb.org/t/p/original',
+  small: 'https://image.tmdb.org/t/p/w500'
 }
 
-function setMainMovie(movieId){
-    fetch(getUrlMovie(movieId)).then(response => response.json()).then(data => {
-        const app = document.getElementById('app')
+const movies = []
+let movieActive = ''
+const moviesElement = document.getElementById('movies')
+
+function getUrlMovie(movieId) {
+  return `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=${API_LANGUAGE}`
+}
+
+function changeButtonMenu() {
+  const button = document.querySelector('.button__menu')
+  const navigation = document.querySelector('.navigation')
+
+
+  button.classList.toggle('active')
+  navigation.classList.toggle('active')
+}
+
+function setMainMovie(movie) {
+  const appImage = document.querySelector('.app__image img')
+  const title = document.querySelector('.feature__movie h1')
+  const description = document.querySelector('.feature__movie p')
+  const info = document.querySelector('.feature__movie span')
+  const rating = document.querySelector('.rating strong')
+
+  title.innerHTML = movie.title
+  description.innerHTML = movie.overview
+  rating.innerHTML = movie.vote_average
+  info.innerHTML = movie.release + ' - ' + movie.genre + ' - Movie'
+
+  appImage.setAttribute('src', movie.image.original)
+}
+
+function changeMovieActiveInList(newMovieActive) {
+  const movieActiveCurrent = document.getElementById(movieActive)
+  movieActiveCurrent.classList.remove('active-movie')
+
+  const movieActiveNew = document.getElementById(newMovieActive)
+  movieActiveNew.classList.add('active-movie')
+
+  movieActive = newMovieActive
+}
+
+function changeMainMovie(movieId) {
+  changeMovieActiveInList(movieId)
+
+  const movie = movies.find(movie => movie.id === movieId)
+
+  if(movie?.id) {
+    setMainMovie(movie)
+    changeButtonMenu()
+  } else  {
+    console.log(movies)
+    console.log('não foi possível achar o filme com o id', movieId)
+  }
+}
+
+function createButtonMovie(movieId) {
+  const button = document.createElement('button')
+  button.setAttribute('onclick', `changeMainMovie('${movieId}')`)
+  button.innerHTML = '<img src="/assets/icon-play-button.png" alt="Icon play button" />'
+
+  return button
+}
+
+function createImageMovie(movieImage, movieTitle) {
+  const divImageMovie = document.createElement('div')
+  divImageMovie.classList.add('movie__image')
+
+  const image = document.createElement('img')
+
+  image.setAttribute('src', movieImage)
+  image.setAttribute('alt', `Imagem do filme ${movieTitle}`)
+  image.setAttribute('loading', 'lazy')
+
+  divImageMovie.appendChild(image)
+
+  return divImageMovie
+}
+
+function addMovieInList(movie) {
+  const movieElement = document.createElement('li')
+  movieElement.classList.add('movie')
+
+  movieElement.setAttribute('id', movie.id)
+
+  const genre = `<span>${movie.genre}</span>`
+  const title = `<strong>${movie.title}</strong>`
+
+  movieElement.innerHTML = genre + title
+  movieElement.appendChild(createButtonMovie(movie.id))
+  movieElement.appendChild(createImageMovie(movie.image.small, movie.title))
+
+  moviesElement.appendChild(movieElement)
+}
+
+async function getMovieData(movieId) {
+  const isMovieInList = movies.findIndex(movie => movie.id === movieId)
+
+  if(isMovieInList === -1) {
+    try {
+      let data = await fetch(getUrlMovie(movieId))
+      data = await data.json()
     
-        const title = document.querySelector(' .movie h1')
-        const description = document.querySelector(' .movie p')
-        const info = document.querySelector(' .movie span')
-        const rating = document.querySelector(' .rating strong')
+      const movieData = {
+        id: movieId,
+        title: data.title,
+        overview: data.overview,
+        vote_average: data.vote_average,
+        genre: data.genres[0].name,
+        release: data.release_date.split('-')[0],
+        image: {
+          original: BASE_URL_IMAGE.original.concat(data.backdrop_path),
+          small: BASE_URL_IMAGE.small.concat(data.backdrop_path),
+        }
+      }
+      movies.push(movieData)
     
-        const yearRelease = data.release_date.split('-')[0]
-    
-        title.innerHTML = data.title
-        description.innerHTML = data.overview
-        rating.innerHTML = data.vote_average
-        info.innerHTML = yearRelease + ' - ' + data.genres[0].name + ' - Movie'
-    
-        const image =  BASE_URL_IMAGE.concat(data.backdrop_path)
-        app.style.backgroundImage  =  `linear-gradient(90.18deg, rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
-    })
+      return movieData
+    } catch(error) {
+      console.log('mensagem de erro:', error.message)
+    }
+  }
+
+  return null
 }
 
-function createButtonMovie(movieId){
-    const button = document.createElement('button')
-    button.setAttribute('onclick', `setMainMovie('${movieId}')`)
-    button.innerHTML = '<img src="/assets/icon-play-button.png" alt="Icon play button">'
-    return button
+function loadMovies() {
+  const LIST_MOVIES = ['tt12801262', 'tt4823776', 'tt0800369', 'tt3896198', 'tt1211837', 'tt1825683']
+  
+  LIST_MOVIES.map(async (movie, index) => {
+    const movieData = await getMovieData(movie)
+
+    addMovieInList(movieData)
+
+    if(index === 0) {
+      setMainMovie(movieData)
+      movieActive = movieData.id
+
+      const movieActiveNew = document.getElementById(movieActive)
+      movieActiveNew.classList.add('active-movie')
+    }
+  })
 }
 
-function createMovie(movieId){
-    fetch(getUrlMovie(movieId)).then(response => response.json()).then(data => {
-        const movie = document.createElement('li')
-        const genre = `<span>${data.genres[0].name}</span>`
-        const title = `<strong>${data.title}</strong>`
-        const image =  BASE_URL_IMAGE.concat(data.backdrop_path)
+const buttonAddMovie = document.getElementById('add__movie')
 
-
-        movie.innerHTML = genre + title
-        movie.appendChild(createButtonMovie(movieId))
-        movie.style.backgroundImage = `linear-gradient(180deg, rgba(14, 23, 47, 0.0001) 11.72%, #0E172F 100%), url('${image}')`
-        moviesList.appendChild(movie)
-    })
+function formattedMovieId(movieId) {
+  if(movieId.includes('https://www.imdb.com/title/')) {
+    const id = movieId.split('/')[4]
+    return id
+  }
+  
+  return movieId
 }
 
-function loadListMovies(){
-    LIST_MOVIES.map(createMovie)
-}
+buttonAddMovie.addEventListener('submit', async function(event) {
+  event.preventDefault()
 
+  const newMovieId = formattedMovieId(event.target['movie'].value)
+  const newMovie = await getMovieData(newMovieId)
 
-loadListMovies()
-// Script para inicializar os dados do filme principal
-setMainMovie(LIST_MOVIES[0])
+  if(newMovie?.id) {
+    addMovieInList(newMovie)
+  }
+
+  event.target['movie'].value = ''
+})
+
+loadMovies()

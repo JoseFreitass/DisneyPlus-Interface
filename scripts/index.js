3d39faa67b8642835bf8667e2088f48d8524b70e
JoseFreitass <85848930+JoseFreitass@users.noreply.github.com>
2022-08-09 07:36:05 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -64,7 +64,7 @@ function changeMainMovie(movieId) {
 function createButtonMovie(movieId) {
   const button = document.createElement('button')
   button.setAttribute('onclick', `changeMainMovie('${movieId}')`)
-  button.innerHTML = '<img src="/assets/icon-play-button.png" alt="Icon play button" />'
+  button.innerHTML = '<img src="assets/icon-play-button.png" alt="Icon play button" />'
 
   return button
 }
